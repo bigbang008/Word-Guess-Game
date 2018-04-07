ac8057b11b4c39aaf66c3ff20ad6737045a6d749
SarawutM <imagination.mystery@gmail.com>
2018-04-07 01:21:16 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,159 +3,157 @@ window.onload = function () {
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    
-  var categories = 0;         // Array of topics
-  var chosenCategory = 0 ;    // Selected catagory
-  var getHint = 0 ;           // Word getHint
-  var word = 0 ;              // Selected word
-  var geuss = 0 ;             // Guess
-  var geusses = [ ];          // Stored geusses
-  var lives= 0 ;             // Lives
-  var counter = 0 ;          // Count correct geusses
-  var space = 0;            // Number of spaces in word '-'
-  var error = 0;            // Stored wrong letters
-    
+
+  var categories = 0;         
+  var chosenCategory = 0;    
+  var getHint = 0;          
+  var word = 0;              
+  var geuss = 0;             
+  var geusses = [];          
+  var lives = 0;             
+  var counter = 0;          
+  var space = 0;            
+  var error = 0;            
+
   var showLives = document.getElementById("mylives");
   var showCatagory = document.getElementById("scatagory");
   var getHint = document.getElementById("hint");
   var showClue = document.getElementById("clue");
-  
-  
-  
-    // create alphabet 
+
+  // create alphabet 
   var buttons = function () {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
-  
+    myButtons = document.getElementById("buttons");
+    letters = document.createElement("ul");
+
     for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
+      letters.id = "alphabet";
+      list = document.createElement("li");
+      list.id = "letter";
       list.innerHTML = alphabet[i];
       check();
       myButtons.appendChild(letters);
       letters.appendChild(list);
     }
   }
-      
-    
-    // Select Catagory
+
+
+  // Select Catagory
   var selectCat = function () {
     if (chosenCategory === categories[0]) {
       catagoryName.innerHTML = "Category: 80's ";
     } else if (chosenCategory === categories[1]) {
       catagoryName.innerHTML = "Category: Smash Hitz";
-    } 
+    }
   }
-  
-    // Create geusses ul
+
+
   result = function () {
-  wordHolder = document.getElementById('hold');
-  correct = document.createElement('ul');
-  
+    wordHolder = document.getElementById("hold");
+    correct = document.createElement("ul");
+
     for (var i = 0; i < word.length; i++) {
-      correct.setAttribute('id', 'my-word');
-      guess = document.createElement('li');
-      guess.setAttribute('class', 'guess');
+      correct.setAttribute("id", "my-word");
+      guess = document.createElement("li");
+      guess.setAttribute("class", "guess");
       if (word[i] === "-") {
         guess.innerHTML = "-";
         space = 1;
       } else {
         guess.innerHTML = "_";
       }
-  
+
       geusses.push(guess);
       wordHolder.appendChild(correct);
       correct.appendChild(guess);
     }
-}
-    
-    // Show lives
+  }
+
+  // Show lives
   comments = function () {
     showLives.innerHTML = "You have " + lives + " lives";
     if (lives < 1) {
       showLives.innerHTML = "Game Over";
       alert("RIP. YOU'RE DEAD NOW!~ PLEASE TRY AGAIN")
-      }
+    }
+    
     for (var i = 0; i < geusses.length; i++) {
       if (counter + space === geusses.length) {
         showLives.innerHTML = "You Win!";
       }
     }
   }
-  
-      // Animate man
-  var animate = function () {
-    var drawMe = lives ;
-    drawArray[drawMe]();
-  }
-  
-    
-     // Hangman
-  canvas =  function(){
-  
+
+  // Hangman
+  canvas = function () {
+
     myStickman = document.getElementById("stickman");
     context = myStickman.getContext('2d');
     context.beginPath();
     context.strokeStyle = "#fff";
     context.lineWidth = 2;
   };
-    
-  head = function(){
+
+  head = function () {
     myStickman = document.getElementById("stickman");
     context = myStickman.getContext('2d');
     context.beginPath();
-    context.arc(60, 25, 10, 0, Math.PI*2, true);
+    context.arc(60, 25, 10, 0, Math.PI * 2, true);
     context.stroke();
   }
-      
-  draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
-      
+
+  draw = function ($pathFromx, $pathFromy, $pathTox, $pathToy) {
+
     context.moveTo($pathFromx, $pathFromy);
     context.lineTo($pathTox, $pathToy);
-    context.stroke(); 
+    context.stroke();
   }
+
+  frame1 = function () {
+    draw(0, 150, 150, 150);
+  };
+
+  frame2 = function () {
+    draw(10, 0, 10, 600);
+  };
+
+  frame3 = function () {
+    draw(0, 5, 70, 5);
+  };
+
+  frame4 = function () {
+    draw(60, 5, 60, 15);
+  };
+
+  torso = function () {
+    draw(60, 36, 60, 70);
+  };
+
+  rightArm = function () {
+    draw(60, 46, 100, 50);
+  };
+
+  leftArm = function () {
+    draw(60, 46, 20, 50);
+  };
+
+  rightLeg = function () {
+    draw(60, 70, 100, 100);
+  };
+
+  leftLeg = function () {
+    draw(60, 70, 20, 100);
+  };
+
+  drawArray = [rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2, frame1];
+
+  // Animate man
+  var animate = function () {
+    var drawMe = lives;      
+    drawArray[drawMe]();
+    }
   
-    frame1 = function() {
-      draw (0, 150, 150, 150);
-    };
-     
-     frame2 = function() {
-       draw (10, 0, 10, 600);
-     };
-    
-     frame3 = function() {
-       draw (0, 5, 70, 5);
-     };
-    
-     frame4 = function() {
-       draw (60, 5, 60, 15);
-     };
-    
-     torso = function() {
-       draw (60, 36, 60, 70);
-     };
-    
-     rightArm = function() {
-       draw (60, 46, 100, 50);
-     };
-    
-     leftArm = function() {
-       draw (60, 46, 20, 50);
-     };
-    
-     rightLeg = function() {
-       draw (60, 70, 100, 100);
-     };
-    
-     leftLeg = function() {
-       draw (60, 70, 20, 100);
-     };
-    
-  drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
-  
-  
-    // OnClick Function
+
+  // OnClick Function
   check = function () {
     list.onclick = function () {
       var geuss = (this.innerHTML);
@@ -165,73 +163,70 @@ window.onload = function () {
         if (word[i] === geuss) {
           geusses[i].innerHTML = geuss;
           counter += 1;
-        } 
+        }
       }
       var j = (word.indexOf(geuss));
-        if (j === -1) {
-          lives -= 1;
-          comments();
-          animate();
-          var wrongletter = document.getElementById("guesswrong");
-          var charac = document.createTextNode(" " + geuss);
-          wrongletter.appendChild(charac);
-          error++;
-        } else {
-          comments();
-        }
+      if (j === -1) {
+        lives -= 1;
+        comments();
+        animate();
+        var wrongletter = document.getElementById("guesswrong");
+        var charac = document.createTextNode(" " + geuss);
+        wrongletter.appendChild(charac);
+        error++;
+      } else {
+        comments();
+      }
     }
   }
-    
-      
-    // Play
-    play = function () {
-      categories = [
-          ["beat-it", "jump", "call-me", "like-a-prayer"],
-          ["despacito", "shape-of-you", "bodak-yellow", "issues"]
-      ];
-  
-      chosenCategory = categories[Math.floor(Math.random() * categories.length)];
-      word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
-      word = word.replace(/\s/g, "-");
-      console.log(word);
-      buttons();
-  
-      geusses = [ ];
-      lives = 10;
-      counter = 0;
-      space = 0;
-      result();
-      comments();
-      selectCat();
-      canvas();
-    }
-  
+
+
+  // Play
+  play = function () {
+    categories = [
+      ["beat-it", "jump", "call-me", "like-a-prayer"],
+      ["despacito", "shape-of-you", "bodak-yellow", "issues"]
+    ];
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    geusses = [];
+    lives = 9;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    selectCat();
+    canvas();
+  }
+
+  play();
+
+  // Hint  
+  hint.onclick = function () {
+
+    hints = [
+      ["The song became a signature song of Jackson", "It is Van Halen's most successful single to date", "Debbie Harry did the rap", "Make a Wish!"],
+      ["Popular Spanish Song", "Ed sings about falling in love with a girl that he meets at a bar", "Look, I don't dance now. I make money moves", "We always have problems"]
+    ];
+
+    var catagoryIndex = categories.indexOf(chosenCategory);
+    var hintIndex = chosenCategory.indexOf(word);
+    showClue.innerHTML = "Clue:  " + hints[catagoryIndex][hintIndex];
+  };
+
+  // Reset
+  document.getElementById('reset').onclick = function () {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
     play();
-    
-    // Hint
-  
-      hint.onclick = function() {
-  
-        hints = [
-          ["The song became a signature song of Jackson", "It is Van Halen's most successful single to date", "Debbie Harry did the rap", "Make a Wish!"],
-          ["Popular Spanish Song", "Ed sings about falling in love with a girl that he meets at a bar", "Look, I don't dance now. I make money moves", "We always have problems"]
-      ];
-  
-      var catagoryIndex = categories.indexOf(chosenCategory);
-      var hintIndex = chosenCategory.indexOf(word);
-      showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
-    };
-  
-     // Reset
-  
-    document.getElementById('reset').onclick = function() {
-      correct.parentNode.removeChild(correct);
-      letters.parentNode.removeChild(letters);
-      showClue.innerHTML = "";
-      context.clearRect(0, 0, 400, 400);
-      play();
-    }
+  }
 }
-  
-  
-  
+
+
